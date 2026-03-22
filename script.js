106d27261e5fcfa2d7c7d31f47046938322fccaa
annikareddy10 <annikareddy10@gmail.com>
2026-03-22 22:24:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    if(email === "" || password === "") {
+        alert("Fill all fields!");
+        return false;
+    }
+}
